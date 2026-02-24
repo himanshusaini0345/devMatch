f8ef1a9a57e093cd8e5968daf64566b66716c5f8
himanshu <himanshusaini0345@outlook.com>
2026-02-24 12:13:48 +0530

    fix(main): add global validation pipe

--- src/profiles/profiles.controller.ts
+++ src/profiles/profiles.controller.ts
@@ -32,7 +32,7 @@ export class ProfilesController {
 
   // POST /profiles
   @Post()
-  create(@Body(new ValidationPipe()) createProfileDto: CreateProfileDto) {
+  create(@Body() createProfileDto: CreateProfileDto) {
     return this.profilesService.create(
       createProfileDto.name,
       createProfileDto.description,
@@ -40,7 +40,7 @@ export class ProfilesController {
   }
   // PUT /profiles/:id
   @Put(':id')
-  update(@Param('id',ParseUUIDPipe) id: UUID, @Body(new ValidationPipe()) updateProfileDto: UpdateProfileDto) {
+  update(@Param('id',ParseUUIDPipe) id: UUID, @Body() updateProfileDto: UpdateProfileDto) {
     return this.profilesService.update(
       id,
       updateProfileDto.name,
