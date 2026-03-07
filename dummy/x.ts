6a8720842d3cb6ee10e54c6b2fb0a7699115e56a
himanshu <himanshusaini0345@outlook.com>
2026-03-07 18:04:06 +0530

    .

--- dummy/x.ts
+++ dummy/x.ts
@@ -36,5 +36,23 @@
 //     console.log(`${String(key)}:${obj[key]}`);
 // }
 
-const x:unknown = "hello";
-console.log((x as string).length);
+// const x:unknown = "hello";
+// console.log((x as string).length);
+
+// console.log(typeof Array)
+// const arr = new Array(1,2,3)
+// console.log(arr.length);
+// arr.push(4)
+// console.log(Array.prototype.push)
+// const arr2 = structuredClone<Array<unknown>>(arr);
+// const arr3 = [...arr];
+// console.log(arr)
+// arr3.pop()
+// console.log(arr)
+// console.log(arr.hasOwnProperty('length'));
+// console.log(Array.prototype.length);
+// console.log(Array.isArray(Array.prototype));
+const arr = [1, 2, 3];
+console.log(Object.getOwnPropertyDescriptor(arr,"length"))
+// delete arr.length;
+// console.log(arr.length)
